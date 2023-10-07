3a860ef9a76278ce63488bccc501bb88e676c65f
arnavpranjal <91477429+arnavpranjal@users.noreply.github.com>
2023-10-07 21:50:46 +0530

    commit 4

--- loan-app/imports/ui/Signup.jsx
+++ loan-app/imports/ui/Signup.jsx
@@ -5,14 +5,17 @@ import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
   let navigate = useNavigate();
   const handleSignup = () => {
+
     new SimpleSchema({
       email: { type: String, regEx: SimpleSchema.RegEx.Email },
       password: String,
-    }).validate({ email, password });
+      role: { type: String, allowedValues: ['admin', 'borrower', 'lender'] }
+    }).validate({ email, password, role });
 
-    Accounts.createUser({ email, password }, (err) => {
+    Accounts.createUser({ email, password , profile: { role }}, (err) => {
       if (err) {
         alert('Signup Failed.');
       } else {
@@ -26,6 +29,12 @@ const Signup = () => {
     <div class="login-container">
       <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <select value={role} onChange={e => setRole(e.target.value)}>
+        <option value="" disabled>Select your role</option>
+        <option value="admin">Admin</option>
+        <option value="borrower">Borrower</option>
+        <option value="lender">Lender</option>
+      </select>
       <button onClick={handleSignup}>Sign Up</button>
       <button onClick={()=>navigate(-1)}>Go Back Home</button> 
     </div>
