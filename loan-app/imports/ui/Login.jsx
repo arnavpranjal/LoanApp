697c3d516b35a9a2e1f550b0b434a1eb177f690e
arnavpranjal <91477429+arnavpranjal@users.noreply.github.com>
2023-10-07 21:06:50 +0530

    commit2

--- loan-app/imports/ui/Login.jsx
+++ loan-app/imports/ui/Login.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+ 
+  let navigate = useNavigate();
+ 
   const handleLogin = () => {
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         alert('Login Failed. Check your email and password.');
       } else {
         // Redirect or do something on successful login
+
       }
     });
   };
 
   return (
-    <div>
+    <div class = "centering" >
+    <div class="login-container">
       <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
+     
+      <button onClick={()=>navigate("/signup")}>Sign up</button> 
+    </div>
     </div>
   );
 };
--- loan-app/imports/ui/Signup.jsx
+++ loan-app/imports/ui/Signup.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
-
+import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  let navigate = useNavigate();
   const handleSignup = () => {
     new SimpleSchema({
       email: { type: String, regEx: SimpleSchema.RegEx.Email },
@@ -22,10 +22,13 @@ const Signup = () => {
   };
 
   return (
-    <div>
+    <div class="centering">
+    <div class="login-container">
       <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
       <button onClick={handleSignup}>Sign Up</button>
+      <button onClick={()=>navigate(-1)}>Go Back Home</button> 
+    </div>
     </div>
   );
 };
